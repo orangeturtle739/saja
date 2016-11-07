bc65944fb9cee39814a87f3b4dc71ba108b3032a
Amit Mizrahi <amit@amizrahi.com>
2016-11-07 18:45:10 -0500

    .mli, not Struct

--- interfaces/broadcast.mli
+++ interfaces/broadcast.mli
@@ -1,19 +1,15 @@
 open Udp (* From Jane Street *)
 
-module Broadcast = struct
+(* A UDP broadcast sends a message to a "broadcast address" that all
+  users listen to. *)
 
-  (* A UDP broadcast sends a message to a "broadcast address" that all
-    users listen to. *)
+type broadcast_address = string
 
-  type broadcast_address = string
+(* Configures UDP settings. *)
 
-  (* Configures UDP settings. *)
+val set_config: Config -> ()
 
-  val set_config: Config -> ()
+(* Sends a broadcast out to the network and binds a callback to the event
+  that packets are received from the broadcast address. *)
 
-  (* Sends a broadcast out to the network and binds a callback to the event
-    that packets are received from the broadcast address. *)
-
-  val send_broadcast: broadcast_address -> ()
-
-end
+val send_broadcast: broadcast_address -> ()
