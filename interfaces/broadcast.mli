4cd2ea5b1be6c75738f972859cee4c739d7b06bd
Amit Mizrahi <amit@amizrahi.com>
2016-11-07 18:53:14 -0500

    Fix broadcast

--- interfaces/broadcast.mli
+++ interfaces/broadcast.mli
@@ -4,12 +4,20 @@ open Udp (* From Jane Street *)
   users listen to. *)
 
 type broadcast_address = string
+type user_info = {
+  ip_address: string;
+  public_key: string;
+  username: string
+}
 
 (* Configures UDP settings. *)
 
 val set_config: Config -> ()
 
-(* Sends a broadcast out to the network and binds a callback to the event
-  that packets are received from the broadcast address. *)
+(* Sends a broadcast out to the network.
+  Binds a callback to the event that packets are received
+  from the broadcast address which parses the packet
+  and adds the user_info obtained from the packet to a list.
+  After some time, the list is returned. *)
 
-val send_broadcast: broadcast_address -> ()
+val send_broadcast: broadcast_address -> user_info list
