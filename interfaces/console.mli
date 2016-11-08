d4131b79899f797a217c8dd017fd88b434c94ba7
sb846 <sb846@cornell.edu>
2016-11-07 19:19:09 -0500

    comment changes

--- interfaces/console.mli
+++ interfaces/console.mli
@@ -1,9 +1,9 @@
 (* This module controls the I/O processes for the console. *)
 
 (* [read_input ()] reads the string typed bu the user into the console
-   returns the string. *)
+ * returns the string. *)
 val read_input: unit -> message
 
 (* [print_to_console s] prints the string [s] to the console. Returns
-   a unit. *)
+ * a unit. *)
 val print_to_console: message -> unit
