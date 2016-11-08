534c9fae991608b0ae2455294f983d793547d2fe
sb846 <sb846@cornell.edu>
2016-11-07 19:13:38 -0500

    changes to msgtransport

--- interfaces/console.mli
+++ interfaces/console.mli
@@ -2,8 +2,8 @@
 
 (* [read_input ()] reads the string typed bu the user into the console
    returns the string. *)
-val read_input: unit -> string
+val read_input: unit -> message
 
 (* [print_to_console s] prints the string [s] to the console. Returns
    a unit. *)
-val print_to_console: string -> unit
+val print_to_console: message -> unit
