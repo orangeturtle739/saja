5eb94894925c8f35bd99c4ac9b2a0b2d62290a04
Amit Mizrahi <amit@amizrahi.com>
2016-11-13 11:05:47 -0500

    Broadcast

--- src/broadcast.mli
+++ src/broadcast.mli
@@ -1,11 +1,6 @@
-open Udp (* From Jane Street *)
-
 (* [broadcast_address] is the address we send broadcasts to, and the address that
  * all online users listen to. *)
 type broadcast_address = string
-
-(* [set_config config] configures UDP settings. *)
-val set_config: Config -> unit
 
 (* [send_broadcast broadcast_address] sends a
  * broadcast out to the network. It then
@@ -13,5 +8,14 @@ val set_config: Config -> unit
  * from the broadcast address which parses the packet
  * and adds the user_info obtained from the packet to a list.
  * After some time, the list is returned. *)
+val send_broadcast: broadcast_address -> unit
 
-val send_broadcast: broadcast_address -> online_user list
+(* [handle_broadcast] takes a function from online_user to a unit and
+    returns a unit. It binds this function as a callback that processes
+    the information received in a broadcast. *)
+val handle_broadcast: (online_user -> unit) -> unit
+
+(* [respond] takes a function from unit to online_user
+    and returns a unit. It binds this function as a callback
+    that generates the response to a received broadcast. *)
+val respond: (unit -> online_user) -> unit
