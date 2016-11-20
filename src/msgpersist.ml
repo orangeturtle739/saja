fbc2e450678fca8d60f26d2be37553548f69ffba
Alex Libman <kingalexowns@gmail.com>
2016-11-19 23:00:16 -0500

    Implement message persistence

---
+++ src/msgpersist.ml
@@ -0,0 +1,21 @@
+open Yojson.Basic
+open Persistence
+open Data
+
+type chatlog = (username * message) list
+
+(* [writelog file log] writes a chat history [log] to [file]. *)
+let write_log (file: filename) (log: chatlog) =
+	let j = `List (List.map (fun (user, msg) -> 
+							 `List [`String user; `String msg]) log) in
+	write_file file j
+
+let parse_msg (msg: json) =
+	match Util.to_list msg with
+	| [user; entry] -> Util.((to_string user, to_string entry))
+	| _ -> failwith "Improperly stored chat message"
+
+(* [read_log file] returns the chatlog described by a [file]. *)
+let read_log (file: filename): chatlog =
+	let j = from_file file in
+	Util.to_list j |> List.map parse_msg
--- src/msgpersist.mli
+++ src/msgpersist.mli
@@ -1,10 +1,10 @@
 open Persistence
 open Data
 
-type chatlog = (username * string) list
+type chatlog = (username * message) list
 
 (* [writelog file log] writes a chat history [log] to [file]. *)
-val write_log: string -> chatlog -> unit
+val write_log: filename -> chatlog -> unit
 
-(* [read_log] file returns the chatlog described by a [file]. *)
-val read_log: string -> chatlog
+(* [read_log file] returns the chatlog described by a [file]. *)
+val read_log: filename -> chatlog
