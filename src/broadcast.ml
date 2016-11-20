a4eb6969e554baa7c5bc7d7c976cf242b552830d
Amit Mizrahi <amit@amizrahi.com>
2016-11-20 10:14:22 -0500

    Implement recvloop, make code cleaner

--- src/broadcast.ml
+++ src/broadcast.ml
@@ -12,27 +12,40 @@ type broadcast_address = string
  * and adds the user_info obtained from the packet to a list.
  * After some time, the list is returned. *)
 
-(* Citation: This implementation of UDP broadcasts is based on
+let broadcast_string = "BROADCAST"
+let port = 3110
+
+let get_broadcast_address () : string =
+  "10.148.127.255"
+
+(* Citation: This implementation of UDP broadcasts is loosely based on
   https://github.com/hverr/udptun/blob/7483c11c773c92bd4c6022329aebc33a843c64e6/bin/tunnel.ml
+  and
+  https://github.com/tox4j/deprecated-tox4j/blob/a84a28e8c24821407652c2ed3c3bc43a1942a4ee/projects/tox4j/src/main/ocaml/core/network.ml
   *)
 
 let send_broadcast : unit Deferred.t =
   let broadcast_address =
-    (Socket.Address.Inet.create (Unix.Inet_addr.of_string "10.148.127.255") 3110) in
+    (Socket.Address.Inet.create (Unix.Inet_addr.of_string (get_broadcast_address ()) ) port) in
   let socket_fd = Unix.Socket.fd (Unix.Socket.(create Type.udp)) in
-  let buffer = Iobuf.of_string "BROADCAST" in
-  let send_func = Or_error.ok_exn (Udp.sendto()) in
-  try_with ~extract_exn: true
+  let buffer = Iobuf.of_string broadcast_string in
+  let send_func = Or_error.ok_exn (Udp.sendto ()) in
+  try_with ~extract_exn:true
     (fun () -> send_func socket_fd buffer broadcast_address) >>|
       function
       | Ok () -> ()
-      | Error e -> raise e
+      | Error (Unix.Unix_error (err, _, _)) -> print_endline
+        (Core.Std.Unix.error_message err)
 
-(* [handle_broadcast] takes a function from online_user to a unit and
-    returns a unit. It binds this function as a callback that processes
-    the information received in a broadcast. *)
-let handle_broadcast (callback : (Socket.Address.Inet.t -> string -> unit Deferred.t)) : unit Deferred.t =
-  failwith "Unimplemented"
+(* [listen] listens for UDP broadcasts. *)
+let listen : unit Deferred.t =
+  let socket_fd = Unix.Socket.fd (Unix.Socket.(create Type.udp)) in
+  Udp.recvfrom_loop socket_fd (fun message_buffer addr ->
+    let address = Socket.Address.Inet.to_string addr in
+    let message = Iobuf.to_string message_buffer in
+    if message = broadcast_string then
+      print_endline ("Received broadcast from " ^ address)
+    else ())
 
 (* [respond] takes a function from unit to online_user
     and returns a unit. It binds this function as a callback
