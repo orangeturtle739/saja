525e92198607420c50e5dbe2bde6bb83804e9770
Amit Mizrahi <amit@amizrahi.com>
2016-11-20 09:46:12 -0500

    Implement broadcasts

--- src/broadcast.mli
+++ src/broadcast.mli
@@ -1,4 +1,5 @@
 open Data
+open Async.Std
 
 (* [broadcast_address] is the address we send broadcasts to, and the address that
  * all online users listen to. *)
@@ -10,14 +11,14 @@ type broadcast_address = string
  * from the broadcast address which parses the packet
  * and adds the user_info obtained from the packet to a list.
  * After some time, the list is returned. *)
-val send_broadcast: broadcast_address -> unit
+val send_broadcast: unit Deferred.t
 
 (* [handle_broadcast] takes a function from online_user to a unit and
     returns a unit. It binds this function as a callback that processes
     the information received in a broadcast. *)
-val handle_broadcast: (online_user -> unit) -> unit
+(* val handle_broadcast: (online_user -> unit) -> unit *)
 
 (* [respond] takes a function from unit to online_user
     and returns a unit. It binds this function as a callback
     that generates the response to a received broadcast. *)
-val respond: (unit -> online_user) -> unit
+(* val respond: (unit -> online_user) -> unit *)
