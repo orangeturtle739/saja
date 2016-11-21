bc2aae0d96cc223bb42c9eeed3661fd1ea9e6e74
Alex Libman <kingalexowns@gmail.com>
2016-11-20 21:51:56 -0500

    Convert tabs to spaces

--- src/msgpersist.ml
+++ src/msgpersist.ml
@@ -6,18 +6,18 @@ type chatlog = (username * message) list
 
 (* [writelog file log] writes a chat history [log] to [file]. *)
 let write_log (file: filename) (log: chatlog) =
-	let j = `List (List.map (fun (user, msg) -> 
-							 `List [`String user; `String msg]) log) in
-	write_file file j
+    let j = `List (List.map (fun (user, msg) -> 
+                             `List [`String user; `String msg]) log) in
+    write_file file j
 
 (* [parse_msg msg] converts a JSON list representing a message entry
  * into a tuple (user, message). *)
 let parse_msg (msg: json) =
-	match Util.to_list msg with
-	| [user; entry] -> Util.((to_string user, to_string entry))
-	| _ -> failwith "Improperly stored chat message"
+    match Util.to_list msg with
+    | [user; entry] -> Util.((to_string user, to_string entry))
+    | _ -> failwith "Improperly stored chat message"
 
 (* [read_log file] returns the chatlog described by a [file]. *)
 let read_log (file: filename): chatlog =
-	let j = from_file file in
-	Util.to_list j |> List.map parse_msg
+    let j = from_file file in
+    Util.to_list j |> List.map parse_msg
--- src/persistence.ml
+++ src/persistence.ml
@@ -5,9 +5,9 @@ type filename = string
 
 (* [write_file file contents] writes JSON [contents] into [file].*)
 let write_file (file: filename) (j: json) =
-	to_file file j
+    to_file file j
 
 (* [read_file file] returns the JSON contents of a [file]. If [file] cannot
  * be found, raises a [Sys_error]. *)
 let read_file (file: filename) =
-	from_file file
+    from_file file
