81b5e56655405018a9eddf9122c3d92e42cc0ba1
Amit Mizrahi <amit@amizrahi.com>
2016-11-20 22:55:44 -0500

    Make more modular, send username along with public key

--- src/broadcast.ml
+++ src/broadcast.ml
@@ -6,37 +6,37 @@ open Async.Std
 let broadcast_string = "BROADCAST"
 let udp_port = 3110
 let exchange_port = 5999
+let username = "amit" (* TODO replace with username specified in controller *)
+let my_key = {n="test"; e="fake"; d="bogus"} (* TODO: use (retrieve_user_key (load_keystore())) in Controller *)
 
 (* [get_broadcast_addresses] gets the computer's broadcast addresses. *)
 let get_broadcast_addresses () : string list =
   ["10.148.127.255"] (* TODO replace with scraped output from ifconfig *)
 
-(* [serialize_public_key] serializes a public key record into a string. *)
-let serialize_public_key (key : full_key) : string =
-  key.n ^ " " ^ key.e
+(* [serialize_user_info] serializes a username and a key into a string. *)
+let serialize_user_info (username: string) (key : full_key) : string =
+  username ^ " " ^ key.n ^ " " ^ key.e
 
-(* The public key string looks like n^" "^e. [deserialize_public_key] unpacks this into a public key record. *)
-let deserialize_public_key str =
-  let tokens = String.split str ~on:' ' in
-    match tokens with
-      | [n; e] -> {n=n; e=e}
-      | _ -> failwith "Malformatted public key string."
+(* The user info string looks like username^" "^n^" "^e. [deserialize_user_info] unpacks this into a user record.
+    Returns unit for now. *)
+let deserialize_user_info (msg: string) (addr: string) : unit =
+  let _ =
+    let tokens = String.split msg ~on:' ' in
+      match tokens with
+        | [u; n; e] -> let p = {n=n; e=e} in
+          let user = {
+            username=u;
+            public_key=p}
+          in
+          {user=user; ip_address=addr}
+        | _ -> failwith "Malformatted user info message."
+  in ()
 
 (* Citation: This implementation of UDP broadcasts is loosely based on
   https://github.com/hverr/udptun/blob/7483c11c773c92bd4c6022329aebc33a843c64e6/bin/tunnel.ml
   and
   https://github.com/tox4j/deprecated-tox4j/blob/a84a28e8c24821407652c2ed3c3bc43a1942a4ee/projects/tox4j/src/main/ocaml/core/network.ml
   *)
-
-let assemble_online_user addr public_key =
-  let _ =
-  {
-    user={
-      username="OnlineUser"; (* TODO replace this with username from persistence modules *)
-      public_key=public_key
-    };
-    ip_address=addr
-  } in ()
 
 (* [setup_exchange_server] sets up a TCP server that listens for messages from
     respondents to a broadcast. *)
@@ -49,9 +49,8 @@ let setup_exchange_server : unit Deferred.t =
       (* [buffer] contains a respondent's public key. Need to store in the keystore if it's not there *)
       | `Eof -> failwith "EOF"
       | `Ok msg ->  (let addr_string = Socket.Address.Inet.to_string addr in
-                    let public_key = deserialize_public_key buffer in
-      assemble_online_user addr_string public_key); return () (* TODO store user details in keypersist *)
-    >>= fun () -> Writer.write w (serialize_public_key (retrieve_user_key (load_keystore()))); Writer.flushed w >>= fun () -> read_responses_callback addr r w
+                      deserialize_user_info addr_string buffer); return () (* TODO store user details in keypersist from Controller *)
+    >>= fun () -> Writer.write w (serialize_user_info username my_key); Writer.flushed w >>= fun () -> read_responses_callback addr r w
   in
   let server = Tcp.Server.create socket
   in return(ignore(server))
@@ -62,7 +61,7 @@ let setup_exchange_server : unit Deferred.t =
 let setup_exchange_client (addr: string) : unit Deferred.t =
   let conn = Tcp.to_host_and_port addr exchange_port in
   Tcp.connect conn  >>= fun (addr,r,w) ->
-    Writer.write w (serialize_public_key (retrieve_user_key (load_keystore())));
+    Writer.write w (serialize_user_info username my_key);
     Writer.flushed w >>= fun () ->
       (* Read in response *)
       let buffer = String.create (128) in
