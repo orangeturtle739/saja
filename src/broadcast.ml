93cd52719ccc3e909a5420ef2b9c706f5a2c9eb3
Jacob Glueck <swimgiraffe435@gmail.com>
2016-11-22 08:55:46 -0500

    Start to clean up broadcast

--- src/broadcast.ml
+++ src/broadcast.ml
@@ -5,103 +5,50 @@ open Async_unix
 
 let broadcast_string = "gem-broadcast"
 let udp_port = 31100
-let exchange_port = 59999
-let username = "amit" (* TODO replace with username specified in controller *)
-let my_key = {n="test"; e="fake"; d="bogus"} (* TODO: use (retrieve_user_key (load_keystore())) in Controller *)
+let discovery_callback = ref (fun _ -> ())
 
 (* [broadcast_address] gets the computer's broadcast addresses. *)
 let broadcast_address : string = "255.255.255.255"
 
-(* [serialize_user_info] serializes a username and a key into a string. *)
-let serialize_user_info (username: string) (key : full_key) : string =
-  username ^ " " ^ key.n ^ " " ^ key.e
-
-(* The user info string looks like username^" "^n^" "^e. [deserialize_user_info] unpacks this into a user record.
-    Returns unit for now. *)
-let deserialize_user_info (msg: string) (addr: string) : unit =
-  let _ =
-    let tokens = String.split msg ~on:' ' in
-    match tokens with
-    | [u; n; e] -> let p = {n=n; e=e} in
-      let user = {
-        username=u;
-        public_key=p}
-      in
-      {user=user; ip_address=addr}
-    | _ -> failwith "Malformatted user info message."
-  in ()
-
-(* Citation: This implementation of UDP broadcasts is loosely based on
-   https://github.com/hverr/udptun/blob/7483c11c773c92bd4c6022329aebc33a843c64e6/bin/tunnel.ml
-   and
-   https://github.com/tox4j/deprecated-tox4j/blob/a84a28e8c24821407652c2ed3c3bc43a1942a4ee/projects/tox4j/src/main/ocaml/core/network.ml
-*)
-
-(* [setup_exchange_server] sets up a TCP server that listens for messages from
-    respondents to a broadcast. *)
-let setup_exchange_server () : unit Deferred.t =
-  let socket = print_endline "going to make socket"; Tcp.on_port exchange_port in
-  let rec read_responses_callback = print_endline "made socket"; fun addr r w ->
-      (* Callback when message received from client *)
-      let buffer = String.create (128) in
-      Reader.read r buffer >>= function
-        (* [buffer] contains a respondent's public key. Need to store in the keystore if it's not there *)
-      | `Eof -> print_endline "EOF????"; failwith "EOF"
-      | `Ok msg -> print_endline "OK msg?"; (let addr_string = Socket.Address.Inet.to_string addr in
-                                             deserialize_user_info addr_string buffer); return () (* TODO store user details in keypersist from Controller *)
-        >>= fun () -> Writer.write w (serialize_user_info username my_key); Writer.flushed w >>= fun () -> read_responses_callback addr r w
-  in
-  let server = Tcp.Server.create socket
-  in return(ignore(server))
-
-(* [setup_exchange_client] establishes a TCP connection to an address [addr] that
-    has sent a broadcast to the client. It then sends information over this
-    TCP connection. *)
-let setup_exchange_client (addr: string) : unit Deferred.t =
-  let conn = Tcp.to_host_and_port addr exchange_port in
-  Tcp.connect conn  >>= fun (addr,r,w) ->
-  Writer.write w (serialize_user_info username my_key);
-  Writer.flushed w >>= fun () ->
-  (* Read in response *)
-  let buffer = String.create (128) in
-  Reader.read r buffer >>= function
-    (* [buffer] contains the new user's public key. Need to store in the keystore if it's not there *)
-  | `Eof -> failwith "EOF"
-  | `Ok msg -> return (print_endline buffer)
-    >>= fun () -> return ()
-
-(* [send_broadcast] sends a broadcast and sets up a TCP server
-    to listen for information sent from respondents to the broadcast. *)
-let rec send_broadcast (address : string) : unit Deferred.t =
+let rec send_broadcast (address : string) : bool Deferred.t =
+  print_endline ":((()))";
   let broadcast_address =
-    (Socket.Address.Inet.create (Unix.Inet_addr.of_string address) udp_port) in print_endline "foo 1";
+    (Socket.Address.Inet.create (Unix.Inet_addr.of_string address) udp_port) in
   let socket = Std.Socket.create Std.Socket.Type.udp in
   Std.Socket.setopt socket Std.Socket.Opt.broadcast true;
-  let buffer = Iobuf.of_string broadcast_string in print_endline "foo 3";
-  let send_func = Or_error.ok_exn (Udp.sendto ()) in print_endline "foo 4";
-  Std.Socket.bind socket (Std.Socket.Address.Inet.create_bind_any 0) >>= fun sock ->
+  let buffer = Iobuf.of_string broadcast_string in
+  let send_func = Or_error.ok_exn (Udp.sendto ()) in
+  Std.Socket.(bind socket (Address.Inet.create_bind_any 0)) >>= fun sock ->
   (fun () -> send_func (Std.Socket.fd sock) buffer broadcast_address) |>
   try_with >>| (function
-      | Ok () -> print_endline "Sent."
-      | Error (Unix.Unix_error (err, _, _)) -> print_endline "Error";
-        Core.Std.Unix.error_message err |> print_endline) >>= fun _ ->
-  Async.Std.after (Core.Std.sec 1.) >>= fun _ ->
-  send_broadcast address
+      | Ok () -> true
+      | Error (Unix.Unix_error (err, _, _)) -> false)
 
-(* [listen_for_broadcast] listens for UDP broadcasts. *)
+let bind_discovery callback = discovery_callback := callback
+
+(* [listen_for_broadcast] listens for UDP broadcasts and invokes the
+ * discovery_callback if needed *)
 let listen_for_broadcast () : unit Deferred.t =
-  print_endline "Started listening.";
+  print_endline "starting";
   let socket = Std.Socket.create Std.Socket.Type.udp in
-  Std.Socket.bind socket (Std.Socket.Address.Inet.create_bind_any udp_port) >>= fun sock ->
+  Std.Socket.(bind socket (Address.Inet.create_bind_any udp_port)) >>= fun sock ->
+  print_endline "foo";
   Udp.recvfrom_loop (Std.Socket.fd sock) (fun message_buffer addr ->
-      print_endline "Got it.";
       let address = Socket.Address.Inet.to_string addr in
       let message = Iobuf.to_string message_buffer in
       if message = broadcast_string then
-        print_endline address
+        let _ = Format.printf "Found peer at sad : %s\n" address in
+        (!discovery_callback address)
       else ())
 
-let _ = send_broadcast broadcast_address
+(* Start listening for broadcasts *)
 let _ = listen_for_broadcast ()
+
+(* Demo code. Should be removed before submitting otherwise it will cause
+ * problems *)
+let _ = bind_discovery (fun ip -> Format.printf "Found peer at: %s\n" ip)
+let _ = Core.Std.sec 1. |> after >>= fun _ ->
+  send_broadcast broadcast_address >>| fun ok ->
+  if ok then print_endline "OK!" else print_endline "bad"
 
 let _ = Scheduler.go()
--- src/broadcast.mli
+++ src/broadcast.mli
@@ -2,19 +2,10 @@ open Data
 open Async.Std
 
 (* [send_broadcast broadcast_address] sends a
- * broadcast out to the network. It then
- * binds a callback to the event that packets are received
- * from the broadcast address which parses the packet
- * and adds the user_info obtained from the packet to a list.
- * After some time, the list is returned. *)
-val send_broadcast: ip_address -> unit Deferred.t
+ * broadcast out to the network.
+ * returns: true if the broadcast was successfull, false otherwise *)
+val send_broadcast: ip_address -> bool Deferred.t
 
-(* [handle_broadcast] takes a function from online_user to a unit and
-    returns a unit. It binds this function as a callback that processes
-    the information received in a broadcast. *)
-(* val handle_broadcast: (online_user -> unit) -> unit *)
-
-(* [respond] takes a function from unit to online_user
-    and returns a unit. It binds this function as a callback
-    that generates the response to a received broadcast. *)
-(* val respond: (unit -> online_user) -> unit *)
+(* [bind_discovery func] takes a function from ip_address to unit to be called
+ * whenever a peer is discovered. The function takes the peer's IP address. *)
+val bind_discovery: (ip_address -> unit) -> unit
