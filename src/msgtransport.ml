5ce1a5ddd4f0859fa8fcd4b6728fde16f9ae0bbe
sb846 <sb846@cornell.edu>
2016-11-21 21:20:04 -0500

    msgtransport changes

--- src/msgtransport.ml
+++ src/msgtransport.ml
@@ -1,32 +1,29 @@
-open Core.Std
 open Async.Std
 open Data
 
 (* ================================================================= *)
 
-let relay_packets buffer r w =
-  Reader.read r buffer
+let relay_packets ibuf r w =
+  Reader.read r ibuf
   >>= function
   | `Eof        -> return ()
   | `Ok str_len ->
-    Writer.write w buffer ~len:str_len;
-    print_endline (String.sub buffer 0 str_len);
+    Writer.write w ibuf ~len:str_len;
+    print_endline (String.sub ibuf 0 str_len); (* replace with callback *)
     Writer.flushed w
 
 let send_msg ip port msg =
-  Tcp.connect (Tcp.to_host_and_port ip port) >>=
-  fun (_,_,w) -> Writer.write w msg;
-  Writer.flushed w >>= fun () -> return true
+  try
+    Tcp.connect (Tcp.to_host_and_port ip port) >>=
+    fun (_,_,w) -> Writer.write w msg;
+    Writer.flushed w >>= fun () -> return true
+  with _ -> return false
 
 let listen () =
   print_endline "Welcome";
   let terminal = Tcp.on_port 12999 in
   let _server  = Tcp.Server.create terminal
-  (fun _ r w -> let buffer = String.create 4096 in
-   relay_packets buffer r w) in ()
-
-let _ = listen ()
+  (fun _ r w -> let ibuf = String.make 4096 '.' in
+   relay_packets ibuf r w) in ()
 
 (* send_msg "localhost" 12999 "556688 Crpytic Message from Jacob Glueck!"*)
-
-let _ = Scheduler.go()
