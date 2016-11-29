f07f02a6dc128533e4e0ecd6c06e214a820440ca
Amit Mizrahi <amit@amizrahi.com>
2016-11-29 09:18:41 -0500

    Yes/no

--- src/console.ml
+++ src/console.ml
@@ -17,4 +17,22 @@ let print_error (error : string) : unit =
 let print_system (msg : string) : unit =
   ANSITerminal.(print_string [yellow] msg)
 
-let read_yes_no () = failwith "foo"
+let rec read_yes_no () : bool Async.Std.Deferred.t =
+
+  let yes = function
+    | "y" | "yes" -> true
+    | _ -> false
+  in
+
+  let no = function
+    | "n" | "no" -> true
+    | _ -> false
+  in
+
+  Async.Std.(
+    Reader.read_line stdin >>= function
+      | `Ok s -> let lower = String.lowercase_ascii s in
+          if yes lower then return true
+          else if no lower then return false
+          else read_yes_no()
+      | `Eof -> failwith "Uh-oh!")
