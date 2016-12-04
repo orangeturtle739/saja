596be880fc811528d0d693b4519ab0fb7d084beb
Amit Mizrahi <amit@amizrahi.com>
2016-12-03 22:17:35 -0500

    Charter + specs

--- src/chat.mli
+++ src/chat.mli
@@ -2,20 +2,44 @@ open Data
 
 type t
 
+(* [join session_id users] joins a chat with a given session ID and list of users. *)
 val join: session_id -> online_user list -> t
+
+
+(* [check_join] verifies a session ID and joins a session. *)
 val check_join: session_id -> online_user list ->
   (session_id * (online_user list)) option
+
+(* [create] creates a session based on an online user list. *)
 val create: online_user list -> t
+
+(* [receive_msg] updates the state based on a received message. *)
 val receive_msg: online_user -> session_id -> string -> t -> t option
+
+(* [receive_join] updates the state based on a received "join" message. *)
 val receive_join: online_user -> session_id -> t -> t option
+
+(* [receive_exit] updates the state based on a received "exit" message. *)
 val receive_exit: online_user -> session_id -> t -> t option
+
+(* [send_msg] *)
 val send_msg: string -> string -> t ->
   Message.body * t * ((session_id * username * ip_address) list)
+
+(* [send_init] *)
 val send_init: string -> t ->
   Message.body * t * ((session_id * username * ip_address) list)
+
+(* [send_join] *)
 val send_join: string -> t ->
   Message.body * t * ((session_id * username * ip_address) list)
+
+(* [send_exit] *)
 val send_exit: string -> t ->
   Message.body * t * ((session_id * username * ip_address) list)
+
+(* [info] *)
 val info: t -> (username * ip_address) list
+
+(* [msg_log] *)
 val msg_log: t -> (username * message) list
--- src/console.mli
+++ src/console.mli
@@ -12,21 +12,32 @@ val read_input: unit -> message Deferred.t
  * is one of the above values. *)
 val read_yes_no: unit -> bool Deferred.t
 
+(* [print_error] prints an error in red. *)
 val print_error :  string -> unit
+
+(* [printf_error] is a printf-like function for [print_error]. *)
 val printf_error : ('a, unit, string, unit) format4 -> 'a
 
+(* [print_system] prints a system message in yellow. *)
 val print_system :  string -> unit
+
+(* [printf_system] is a printf-like function for [print_system]. *)
 val printf_system : ('a, unit, string, unit) format4 -> 'a
 
+(* [print_message] prints a message in cyan. *)
 val print_message: string -> unit
+
+(* [printf_message] is a printf-like function for [print_message]. *)
 val printf_message : ('a, unit, string, unit) format4 -> 'a
 
-(* [print_normal message] prints a message to the console. *)
-val print_normal: string -> unit
-val printf_normal : ('a, unit, string, unit) format4 -> 'a
-
+(* [print_username] prints a username. *)
 val print_username: string -> unit
+
+(* [printf_username] is a printf-like function for [print_username]. *)
 val printf_username: ('a, unit, string, unit) format4 -> 'a
 
+(* [print_prompt] prints the prompt ('>>='). *)
 val print_prompt: string -> unit
+
+(* [printf_prompt] is a printf-like function for [print_prompt]. *)
 val printf_prompt: ('a, unit, string, unit) format4 -> 'a
