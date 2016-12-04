bb2ac41dfc9d404e407fdc70d60ac879809aa5db
Alex Libman <kingalexowns@gmail.com>
2016-12-03 22:57:47 -0500

    Don't crash on malformed json

--- src/persistence.mli
+++ src/persistence.mli
@@ -7,12 +7,14 @@ val write_file: filename -> json -> string -> unit
 
 (* [read_file file pass] returns the JSON contents of a [file] decrypted
  * with [pass]. If [file] cannot be found, raises a [Sys_error].
- * If the password is bad, raiess a [Bad_password] *)
+ * If the password is bad, raises a [Bad_password]. If the JSON is
+ * malformed, raises a [Malformed_file]. *)
 val read_file: filename -> string -> json
 
 (* [unenc_write_file file contents] writes JSON [contents] into [file].*)
 val unenc_write_file: filename -> json -> unit
 
-(* [unenc_read_file file] returns the JSON contents of a [file]. If [file] cannot
- * be found, raises a [Sys_error]. *)
+(* [unenc_read_file file] returns the JSON contents of a [file]. If [file]
+ * cannot be found, raises a [Sys_error]. If the JSON is malformed, 
+ * raises a [Malformed_file] *)
 val unenc_read_file: filename -> json
